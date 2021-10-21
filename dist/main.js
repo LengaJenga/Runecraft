1a1f72c1f1a74df03f83f24af5eaa7d054b89335
LengaJenga <matthew.leiher@gmail.com>
2021-10-21 00:35:45 -0400

    more dist fixes

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-import './style.css'
+import 'dist/style.css'
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { Color, FlatShading, Texture, TextureLoader } from 'three';
