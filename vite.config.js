e465c1fa5640e459d9b0ad2ecf75a029c9fcac10
LengaJenga <uberdigger77@live.co.uk>
2025-04-29 17:04:28 -0400

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-// Vite config for GitHub Pages deployment
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // Set the base path to your repo name for correct asset loading on GitHub Pages
-  base: '/Runecraft/',
-});
